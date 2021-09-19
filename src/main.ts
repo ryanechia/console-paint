82511e652d6aa4e669dfd82cfd7d931c875fa54b
Ryane Chia <ryanechia@outlook.com>
2021-09-19 16:15:48 +0800

    add quick help

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,32 @@ const readline = createInterface({
   output: process.stdout
 });
 
+function showHelp(): void {
+  console.log(`
+  Commands Supported: \n
+  Q, q                       Quit Program\n 
+  H, h, Help, help           Quit Program\n 
+  `);
+}
+
+
 function initLoop(): void {
   readline.question('Input command: ', function (userInput: string) {
-        switch (userInput) {
-          case 'q':
-          case 'Q':
-            console.log('Exiting Program');
-            readline.close();
-            break;
-          default:
-            initLoop();
-        }
+      switch (userInput) {
+        case 'q':
+        case 'Q':
+          console.log('Exiting Program');
+          readline.close();
+          break;
+        case 'h':
+        case 'H':
+        case 'Help':
+        case 'help':
+        default:
+          showHelp();
+          initLoop();
+          break;
+      }
   });
 }
 
