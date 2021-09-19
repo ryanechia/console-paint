2695fd91c4b0d2fc0528067bd9b0d7e98bfaeca2
Ryane Chia <ryanechia@outlook.com>
2021-09-19 17:33:48 +0800

    create models for commands

---
+++ src/models/command.model.ts
@@ -0,0 +1,28 @@
+export interface ICommand {
+  command: string
+}
+
+export interface DrawCanvasCommand extends ICommand{
+  rows: number,
+  cols: number
+}
+
+export interface LineCommand extends ICommand{
+  x1: number,
+  x2: number,
+  y1: number,
+  y2: number
+}
+
+export interface RectangleCommand extends ICommand{
+  x1: number,
+  x2: number,
+  y1: number,
+  y2: number
+}
+
+export interface FillCommand extends ICommand{
+  x1: number,
+  x2: number,
+  fillWith: string,
+}
