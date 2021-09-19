56e04352bf954d75e60e3e806302938bd7aa608d
Ryane Chia <ryanechia@outlook.com>
2021-09-20 02:27:50 +0800

    fix syntax for interfaces

--- src/models/command.model.ts
+++ src/models/command.model.ts
@@ -1,28 +1,28 @@
 export interface ICommand {
-  command: string
+  command: string;
 }
 
-export interface DrawCanvasCommand extends ICommand{
-  rows: number,
-  cols: number
+export interface DrawCanvasCommand extends ICommand {
+  rows: number;
+  cols: number;
 }
 
-export interface LineCommand extends ICommand{
-  x1: number,
-  x2: number,
-  y1: number,
-  y2: number
+export interface LineCommand extends ICommand {
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
 }
 
-export interface RectangleCommand extends ICommand{
-  x1: number,
-  x2: number,
-  y1: number,
-  y2: number
+export interface RectangleCommand extends ICommand {
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
 }
 
-export interface FillCommand extends ICommand{
-  x: number,
-  y: number,
-  fillWith: string,
+export interface FillCommand extends ICommand {
+  x: number;
+  y: number;
+  fillWith: string;
 }
