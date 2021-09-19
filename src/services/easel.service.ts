c4da9f86f0dc7e6def33bc103bf6a5ab5ba9b332
Ryane Chia <ryanechia@outlook.com>
2021-09-19 20:31:20 +0800

    implement easel.initCanvas

--- src/services/easel.service.ts
+++ src/services/easel.service.ts
@@ -1,8 +1,36 @@
-let canvas: number[][];
+
+let canvas: string[][];
+
+function printCanvas(): void {
+  canvas.forEach((row: string[]) => {
+    let canvasRow = '';
+    row.forEach((column: string) => {
+      canvasRow += column;
+    })
+    console.log(canvasRow);
+  });
+}
 
 export const initCanvas = (rows: number, columns: number): void => {
-  console.log(rows);
-  console.log(columns);
+  console.log(`rows: ${rows}`);
+  console.log(`columns: ${columns}`);
+
+  // pad by 2 to make room for the border
+  canvas = new Array(rows+2).fill(' ')
+    .map(()=> new Array(columns+2).fill(' '));
+
+  // draw the canvas border
+  canvas.forEach((row: string[], rowIndex: number) => {
+    row.forEach((column: string, colIndex: number) => {
+      if (colIndex === 0 || colIndex === row.length - 1 ) {
+        canvas[rowIndex][colIndex] = '|';
+      }
+      if (rowIndex === 0 || rowIndex === canvas.length - 1 ) {
+        canvas[rowIndex][colIndex] = '-';
+      }
+    })
+  });
+  printCanvas();
 };
 
 export const getCanvas = ():  number[][] | undefined => {
