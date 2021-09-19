b0442ad5606a061228bdf547fae62338362e4f6d
Ryane Chia <ryanechia@outlook.com>
2021-09-19 16:10:29 +0800

    simple run loop

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+import { createInterface } from 'readline';
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function initLoop(): void {
+  readline.question('Input command: ', function (userInput: string) {
+        switch (userInput) {
+          case 'q':
+          case 'Q':
+            console.log('Exiting Program');
+            readline.close();
+            break;
+          default:
+            initLoop();
+        }
+  });
+}
+
+initLoop();
