3e1a794900d5b17ca10e1503c7a1fc0ee6a5290c
Ryane Chia <ryanechia@outlook.com>
2021-09-20 00:52:50 +0800

    move showHelp function into utils file

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,13 @@ import {
   getCanvas,
 } from './services/easel.service';
 import { bucketFill, drawCanvas, drawLine, drawRect } from './services/easel.api';
+import { showHelp } from './utils';
 
 const readline = createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
-
-function showHelp(): void {
-  console.log(`
-  Commands Supported (case-insensitive): \n
-  Q                            Quit Program\n 
-  H, Help                      Show Help\n 
-  C <width> <height>           Create a new canvas of specified width and height.\n
-  L <x1> <y1> <x2> <y2>        Create a new line from (x1,y1) to (x2,y2).\n
-  R <x1> <y1> <x2> <y2>        Create a new rectangle from (x1,y1) to (x2,y2).\n
-  B <x1> <y1> <fill-content>   Paint bucket fill on point (x1,y1) with specified <fill-content></fill-content>.\n
-  ls                           Show the Canvas in all its beautiful glory!\n
-  `);
-}
 
 function parseUserInput(userInput: string): void {
   try {
