7ac3c5301bbfd1dca26e04c5cbaefe1168d454e1
Ryane Chia <ryanechia@outlook.com>
2021-09-19 21:46:04 +0800

    add ls to help text

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ function showHelp(): void {
   L <x1> <y1> <x2> <y2>        Create a new line from (x1,y1) to (x2,y2).\n
   R <x1> <y1> <x2> <y2>        Create a new rectangle from (x1,y1) to (x2,y2).\n
   B <x1> <y1> <fill-content>   Paint bucket fill on point (x1,y1) with specified <fill-content></fill-content>.\n
+  ls                           Show the Canvas in all its beautiful glory!\n
   `);
 }
 
